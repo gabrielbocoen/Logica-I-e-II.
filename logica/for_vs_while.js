0712c75bf3297f40575086119bf2d52f1344ddca
Gabriel Boçoen <86474842+gabrielbocoen@users.noreply.github.com>
2021-12-30 09:35:22 -0300

    Create for_vs_while.js

---
+++ logica/for_vs_while.js
@@ -0,0 +1,16 @@
+//for():
+
+for (var i = 0; i < 10; i++){
+    alert ("O resultado é:" + (2 * i ));
+}
+
+
+//while():
+
+var i = 0;
+while (i = 0){
+  alert ("O resultado é:" + (2 * i ));
+  i++;
+}
+
+//O RESULTADO DOS DOIS SERÁ O MESMO!!!
